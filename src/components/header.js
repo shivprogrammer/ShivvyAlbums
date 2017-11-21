a4540a96c7268d4927c905e4d7faf388a3a10b14
shivprogrammer <shiv.unadkat@gmail.com>
2017-11-21 15:01:11 -0800

    refactored header component for reuse

--- src/components/header.js
+++ src/components/header.js
@@ -3,10 +3,12 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 // Make a component
-const Header = () => {
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles;
+  
   return (
-    <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>Albums!</Text>
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerText}</Text>
     </View>
   );
 };
