2b73a609a54dcd0fcc421a339dd3fafdfd9350fb
shivprogrammer <shiv.unadkat@gmail.com>
2017-11-21 14:44:23 -0800

    more styles to the header

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,20 @@
 // Import libraries for making a component
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 // Make a component
 const Header = () => {
-  const { textStyle } = styles;
-
-  return <Text style={textStyle}>Albums!</Text>;
+  return (
+    <View style={styles.viewStyle}>
+      <Text style={styles.textStyle}>Albums!</Text>
+    </View>
+  );
 };
 
 const styles = {
+  viewStyle: {
+    backgroundColor: '#F8F8F8'
+  },
   textStyle: {
     fontSize: 20
   }
