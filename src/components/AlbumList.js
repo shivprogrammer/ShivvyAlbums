73d8538dadc273a394f66d22680abf58edfc2dfb
shivprogrammer <shiv.unadkat@gmail.com>
2017-11-22 15:43:58 -0800

    adding in AlbumDetail.js

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
   state = { albums: [] };
@@ -11,7 +12,8 @@ class AlbumList extends Component {
   }
 
   renderAlbums() {
-    return this.state.albums.map(album => <Text>{album.title}</Text>);
+    return this.state.albums.map(album =>
+      <Text key={album.title}>{album.title}</Text>);
   }
 
   render() {
