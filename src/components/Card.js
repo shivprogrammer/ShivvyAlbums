e4c920f8b21924a41101006efce07b06115e851e
shivprogrammer <shiv.unadkat@gmail.com>
2017-11-22 16:03:12 -0800

    added ability to reference the children of card components using props [iOS WORKING]

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { View } from 'react-native';
 
-const Card = () => {
+const Card = (props) => {
   const { containerStyle } = styles;
 
   return (
-    <View style={containerStyle}></View>
+    <View style={containerStyle}>
+      {props.children}
+    </View>
   );
 };
 
