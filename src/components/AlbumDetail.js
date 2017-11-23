d4528366456fe8a52021004608ac808cfc0032cf
shivprogrammer <shiv.unadkat@gmail.com>
2017-11-22 16:40:53 -0800

    oh shit the app is really coming togethergit add .git add .! [iOS WORKING]

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -4,12 +4,13 @@ import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image } = album;
+  const { title, artist, thumbnail_image, image } = album;
   const {
     headerContentStyle,
     thumbnailStyle,
     thumbnailContainerStyle,
-    headerTextStyle
+    headerTextStyle,
+    imageStyle
    } = styles;
 
   return (
@@ -25,6 +26,9 @@ const AlbumDetail = ({ album }) => {
           <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image style={imageStyle} source={{ url: image }} />
       </CardSection>
     </Card>
   );
@@ -47,6 +51,11 @@ const styles = {
     alignItems: 'center',
     marginLeft: 10,
     marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 };
 
